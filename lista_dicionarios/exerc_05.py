4bbd912d1f1abbcb3e3f36b411a74c2080f50fcd
suelainearaujo <30813609+suelainearaujo@users.noreply.github.com>
2017-09-20 20:27:41 -0300

    Update exerc_05.py

--- lista_dicionarios/exerc_05.py
+++ lista_dicionarios/exerc_05.py
@@ -1,32 +1,34 @@
-a = 1
 contatos = {}
-while a:
-    opcao = int(input("1 - Inserir\n2 - Remover\n3 - Pesquisar\n4 - Sair\nO que deseja fazer?\n"))
-    if opcao == 1:
-        nome = input("Qual o nome do contato?\n")
-        email = input("Qual o e-mail do contato?\n")
-        telefone = input("Qual o telefone do contato?\n")
-        endereco = input("Qual o endereço do contato?\n")
-        contatos[nome] = [email, telefone, endereco]
-    elif opcao == 2:
-        nome = input("Digite o nome do contato que deseja apagar:\n")
+i=0
+sair=0
+
+while i<=100:
+    resposta = int(input("1 - Inserir\n2 - Remover\n3 - Pesquisar\n4 - Sair\nO que deseja fazer?\n"))
+    if resposta == 1:
+        nome = input('Nome: ')
+        email = input('Email: ')
+        tel = input('Telefone: ')
+        end = input('Endereço: ')
+        contatos[nome] = [email, tel, end]
+        if nome in contatos[nome]:
+            email =input('Atualize o email: ')
+            tel = input('Atualize seu contato: ')
+            end = input('Atualize seu endereço: ')
+    elif resposta == 2:
+        nome=input('Informe o nome que deseja apagar: ')
         del contatos[nome]
-    elif opcao == 3:
-        nome = input("Digite o nome do contato:\n")
+    elif resposta == 3:
+        nome=input('Informe o nome que deseja pesquisar:')
         if nome in contatos:
-            print(nome)
-            j = 0
-            for i in contatos[nome]:
-                if j == 0:
-                    print("E-mail: ", i)
-                elif j == 1:
-                    print("Telefone: ", i)
-                elif j == 2:
-                    print("Endereco: ", i)
-                j += 1
+            print('Email: ',email)
+            print('Telefone: ',tel)
+            print('Endereço: ',end)
         else:
-            print("Contato nao encontrado.")
-    elif opcao == 4:
-        a = 0
+            print('Não foi encontrado nenhum contato com o nome digitado.')
+
+    elif resposta == 4:
+        sair=0
     else:
         print("Opcao invalida.")
+
+i += 1
